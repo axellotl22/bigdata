1d542ac01b4bbc80adda2fbac9a0d9bc60505d96
axellotl <alex.richter39@gmail.com>
2024-02-01 09:22:09 +0100

    database connection

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import os
+from pymongo.mongo_client import MongoClient
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+uri = os.environ['DB_URI']
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
